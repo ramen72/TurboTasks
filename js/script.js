ef7c0f3a595178d46d0153e2b51b7be23bd5df92
Nasar Uddin <dearnasaruddin@gmail.com>
2025-01-10 23:20:58 +0600

    front end completed

--- js/script.js
+++ js/script.js
@@ -1,11 +1,54 @@
-var pendingListItemHolder = document.querySelector("#pendingListItemHolder");
-var completedListItemHolder = document.querySelector("#completedListItemHolder");
-var addNewItemBtn = document.querySelector("#addNewItemBtn");
-var newItemText = document.querySelector("#newItemText");
-var checkedBox = document.querySelector("#checkedBox");
-var deleteIcon = document.querySelector("#deleteIcon");
+let inputBox = document.querySelector("#inputBox")
+let addBtn = document.querySelector("#addBtn")
+let upBtn = document.querySelector("#upBtn")
+let taskItems = document.querySelector("#taskItems")
+let trashItems = document.querySelector(".trash")
 
-var statusDone = document.querySelector("#statusDone");
-var statusPending = document.querySelector("#statusPending");
+let taskList = []
+// let templateArr = `<li>1. ${taskList} <span> <i class="fa-regular fa-pen-to-square"></i> <i class="fa-light fa-trash"></i></span></li>`
+
+let indexNum;
+
+upBtn.addEventListener("click", () => {
+    taskList[indexNum] = inputBox.value
+    console.log(taskList)
+    displayFunc()
+    addBtn.style.display = "inline-block"
+    upBtn.style.display = "none"
+})
 
 
+addBtn.addEventListener("click", () => {
+    // taskList.push( `<li>1. ${inputBox.value} <span> <i class="fa-regular fa-pen-to-square"></i> <i class="fa-light fa-trash"></i></span></li>`)
+    taskList.push(inputBox.value)
+    displayFunc()
+
+
+
+    let editBtn = document.querySelectorAll(".editBtn")
+    let moveTrashBtn = document.querySelectorAll(".moveTrashBtn")
+
+    let editBtnArr = Array.from(editBtn)
+    let moveTrashBtnArr = Array.from(moveTrashBtn)
+
+    editBtnArr.map((item, index) => {
+        item.addEventListener("click", () => {
+            inputBox.value = taskList[index]
+            upBtn.style.display = "inline-block"
+            addBtn.style.display = "none"
+            indexNum = index
+        })
+
+    })
+
+
+})
+
+function displayFunc() {
+    inputBox.value = ""
+    taskItems.innerHTML = ""
+    taskList.map((item, index) => {
+        taskItems.innerHTML += `<li>${index + 1}. ${item} <span> <i class="fa-regular fa-pen-to-square editBtn"></i> <i class="fa-light fa-trash moveTrashBtn"></i></span></li>`
+    })
+    
+}
