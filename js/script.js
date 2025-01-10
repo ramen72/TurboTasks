9d126e5d21eadd13e2097ec4d5bff3765e0dd858
Ramen Biswas <printrb72@gmail.com>
2025-01-10 22:42:06 +0600

    update

--- js/script.js
+++ js/script.js
@@ -1,11 +1,4 @@
-var pendingListItemHolder = document.querySelector("#pendingListItemHolder");
-var completedListItemHolder = document.querySelector("#completedListItemHolder");
-var addNewItemBtn = document.querySelector("#addNewItemBtn");
-var newItemText = document.querySelector("#newItemText");
-var checkedBox = document.querySelector("#checkedBox");
-var deleteIcon = document.querySelector("#deleteIcon");
+const pendingListItemHolder = document.querySelector("#pendingListItemHolder");
+const completeList = document.querySelector("#completeList");
 
-var statusDone = document.querySelector("#statusDone");
-var statusPending = document.querySelector("#statusPending");
-
-
+const completeListArray = []
