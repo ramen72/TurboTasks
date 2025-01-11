5f38e64dd761b9c32c755dcea8ba6ed43b174e57
Nasar Uddin <dearnasaruddin@gmail.com>
2025-01-11 17:59:40 +0600

    JS Done

--- js/script.js
+++ js/script.js
@@ -6,8 +6,6 @@ let trashItems = document.querySelector(".trash")
 
 let taskList = []
 let trashList = []
-// let templateArr = `<li>1. ${taskList} <span> <i class="fa-regular fa-pen-to-square"></i> <i class="fa-light fa-trash"></i></span></li>`
-
 let upIndexNum;
 
 upBtn.addEventListener("click", () => {
@@ -19,32 +17,36 @@ upBtn.addEventListener("click", () => {
 
 
 addBtn.addEventListener("click", () => {
-    // taskList.push( `<li>1. ${inputBox.value} <span> <i class="fa-regular fa-pen-to-square"></i> <i class="fa-light fa-trash"></i></span></li>`)
-    taskList.push(inputBox.value)
-    displayFunc()
+    if (inputBox.value == "" || inputBox.value == " ") {
+        alert("Invalid Task")
+    } else {
+        taskList.push(inputBox.value)
+        displayFunc()
+    }
 
 })
+
 
 
 
 function displayFunc() {
     inputBox.value = ""
     taskItems.innerHTML = ""
+    trashItems.innerHTML = ""
 
     taskList.map((item, index) => {
         taskItems.innerHTML += `<li>${index + 1}. ${item} <span> <i class="fa-regular fa-pen-to-square editBtn"></i> <i class="fa-light fa-trash moveTrashBtn"></i></span></li>`
     })
 
-    trashList.map((item, index)=>{
+    trashList.map((item, index) => {
         trashItems.innerHTML += `<li>${index + 1}. ${item} <span> <i class="fa-solid fa-rotate-right undoBtn"></i> <i class="fa-solid fa-trash delBtn"></i></span></li>`
     })
 
 
-    
+
     let editBtn = document.querySelectorAll(".editBtn")
     let editBtnArr = Array.from(editBtn)
-    
-    
+
     editBtnArr.map((item, index) => {
         item.addEventListener("click", () => {
             inputBox.value = taskList[index]
@@ -53,30 +55,43 @@ function displayFunc() {
             upIndexNum = index
         })
     })
-    
+
+
+
     let moveTrashBtn = document.querySelectorAll(".moveTrashBtn")
     let moveTrashBtnArr = Array.from(moveTrashBtn)
-    
-    moveTrashBtnArr.map((item, index)=>{
-        item.addEventListener("click",()=>{
+
+    moveTrashBtnArr.map((item, index) => {
+        item.addEventListener("click", () => {
             trashList.push(taskList[index])
             taskList.splice(index, 1)
             displayFunc()
         })
     })
-    
-    let undoBtn = document.querySelectorAll(".undoBtn")
-    let undoBtnArr = Array.from(undoBtn)
+
 
 
     let delBtn = document.querySelectorAll(".delBtn")
     let delBtnArr = Array.from(delBtn)
 
-    delBtnArr.map((item, index)=>{
-        item.addEventListener("click", ()=>{
+    delBtnArr.map((item, index) => {
+        item.addEventListener("click", () => {
             trashList.splice(index, 1)
             displayFunc()
         })
     })
-    
+
+
+
+    let undoBtn = document.querySelectorAll(".undoBtn")
+    let undoBtnArr = Array.from(undoBtn)
+
+    undoBtnArr.map((item, index) => {
+        item.addEventListener("click", () => {
+            taskList.push(trashList[index])
+            trashList.splice(index, 1)
+            displayFunc()
+        })
+    })
+
 }
