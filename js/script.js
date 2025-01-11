812f5cb0340e7a2eef9f6a9088b56509530a90f4
Ramen Biswas <printrb72@gmail.com>
2025-01-12 00:11:04 +0600

    Update

--- js/script.js
+++ js/script.js
@@ -1,11 +1,41 @@
-var pendingListItemHolder = document.querySelector("#pendingListItemHolder");
-var completedListItemHolder = document.querySelector("#completedListItemHolder");
-var addNewItemBtn = document.querySelector("#addNewItemBtn");
-var newItemText = document.querySelector("#newItemText");
-var checkedBox = document.querySelector("#checkedBox");
-var deleteIcon = document.querySelector("#deleteIcon");
+const pendingListItemHolder = document.querySelector("#pendingListItemHolder");
+const completedListItemHolder = document.querySelector("#completedListItemHolder");
+const addNewItemBtn = document.querySelector("#addNewItemBtn");
+const newItemText = document.querySelector("#newItemText");
+const checkedBox = document.querySelector("#checkedBox");
+const deleteIcon = document.querySelector("#deleteIcon");
 
-var statusDone = document.querySelector("#statusDone");
-var statusPending = document.querySelector("#statusPending");
+const statusDone = document.querySelector("#statusDone");
+const statusPending = document.querySelector("#statusPending");
 
+const pendingItemArray = [];
 
+addNewItemBtn.addEventListener("click",addItem)
+
+function addItem(){
+    let pendingItem = newItemText.value.trim();
+    if(!pendingItem){
+        return alert("You must input your task")
+    }
+    pendingItemArray.push(pendingItem)
+    displayUpdate()
+    newItemText.value = "";
+    console.log(pendingItemArray);
+}
+
+function displayUpdate() {
+    pendingListItemHolder.innerHTML = "";
+    pendingItemArray.map((item,index)=>{
+        pendingListItemHolder.innerHTML += `<li class="statusPending">${item} <button class="pendingDeleteBtn">Delete</button></li>`
+    })
+
+    const pendingDeleteBtn = document.querySelectorAll(".pendingDeleteBtn");
+    const pendingDeleteBtnArray = Array.from(pendingDeleteBtn)
+    pendingDeleteBtnArray.map((item,index)=>{
+        item.addEventListener("click",()=>{
+            console.log(pendingItemArray);
+            pendingItemArray.splice(index,1)
+            displayUpdate()            
+        })
+    })
+}
