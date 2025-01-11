5662455a309fba9ee9b3b9e0114fe347f20a7fda
Ramen Biswas <133987263+devsramen@users.noreply.github.com>
2025-01-11 16:28:54 +0600

    Update JS part at 11-01-2025_1628

--- js/script.js
+++ js/script.js
@@ -1,11 +1,61 @@
-var pendingListItemHolder = document.querySelector("#pendingListItemHolder");
-var completedListItemHolder = document.querySelector("#completedListItemHolder");
-var addNewItemBtn = document.querySelector("#addNewItemBtn");
-var newItemText = document.querySelector("#newItemText");
-var checkedBox = document.querySelector("#checkedBox");
-var deleteIcon = document.querySelector("#deleteIcon");
+const pendingListItemHolder = document.querySelector("#pendingListItemHolder");
+const completedListItemHolder = document.querySelector("#completedListItemHolder");
+const addNewItemBtn = document.querySelector("#addNewItemBtn");
+const newItemText = document.querySelector("#newItemText");
+const checkedBox = document.querySelector("#checkedBox");
+const deleteIcon = document.querySelector("#deleteIcon");
 
-var statusDone = document.querySelector("#statusDone");
-var statusPending = document.querySelector("#statusPending");
+const statusDone = document.querySelector("#statusDone");
+const statusPending = document.querySelector("#statusPending");
+
+const pendingListArray = ["This is the first Item", "b","c"]
+
+addNewItemBtn.addEventListener("click",()=>{
+    pendingListArray.push(newItemText.value)
+    renderUI()
+    console.log(pendingListArray);
+    
+})
+function renderUI(){
+    pendingListItemHolder.innerHTML = "";
+    pendingListArray.map((item,index)=>{
+        pendingListItemHolder.innerHTML += `<li class="statusPending">
+                            <div class="d-flex justify-content-between align-items-center"><div>${index + 1}
+                               <span>${item}</span></div>
+                                <div class="d-flex justify-content-end align-items-center column-gap-4">
+                                    <button type="button" id="editBtn">Edit</button>
+                                    <button type="button" id="doneBtn">Done</button>
+                                    <i class="fa-solid fa-trash-can"></i>
+                                </div>
+                            </div>
+                        </li>`
+    })
+}
+
+pendingListItemHolder.addEventListener("click",(event)=>{
+    console.log(event.target.parentElement.parentElement.span);
+    
+    // console.log((event.target.parentElement).parentElement.textContent);
+    
+})
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.addEventListener("load",renderUI)
